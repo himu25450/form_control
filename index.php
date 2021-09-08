4828d5387a783845bb20dd24a2f8a4440b3cd94e
himu25450 <himu25450@gmail.com>
2021-09-08 08:59:02 +0530

    form commit

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>test form</title>
+</head>
+<body>
+	<form action="index.php" method="post">
+	<input type="text" name="name">
+	<label for="name">Enter Your Name</label>
+	<input type="email" name="email" id="">
+	<label for="email">Enter Your Email</label>
+	<input type="submit" value="submit">
+	</form>
+</body>
+</html>
